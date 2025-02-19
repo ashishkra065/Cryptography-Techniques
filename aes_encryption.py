97667250f2668f479fc4b8c406d38af03f98a21e
ashishkra065 <107018551+ashishkra065@users.noreply.github.com>
2025-02-19 12:16:39 +0530

    Create aes_encryption.py

---
+++ aes_encryption.py
@@ -0,0 +1,14 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+
+def encrypt_aes(plaintext, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    ciphertext = cipher.encrypt(pad(plaintext.encode(), AES.block_size))
+    return cipher.iv + ciphertext
+
+def decrypt_aes(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = unpad(cipher.decrypt(ciphertext[AES.block_size:]), AES.block_size)
+    return plaintext.decode()
