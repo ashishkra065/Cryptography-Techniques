e4812e591a6131d278e4411586897cb030ea5204
ashishkra065 <107018551+ashishkra065@users.noreply.github.com>
2025-02-19 12:17:10 +0530

    Create rsa_encryption.py

---
+++ rsa_encryption.py
@@ -0,0 +1,21 @@
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+def generate_rsa_keys():
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
+    return private_key, public_key
+
+def encrypt_rsa(plaintext, public_key):
+    rsa_key = RSA.import_key(public_key)
+    cipher = PKCS1_OAEP.new(rsa_key)
+    ciphertext = cipher.encrypt(plaintext.encode())
+    return ciphertext
+
+def decrypt_rsa(ciphertext, private_key):
+    rsa_key = RSA.import_key(private_key)
+    cipher = PKCS1_OAEP.new(rsa_key)
+    plaintext = cipher.decrypt(ciphertext)
+    return plaintext.decode()
